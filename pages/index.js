4956ef9347176db80164cb923c3a1bdceb56af32
Aman <krishanaman14@gmail.com>
2023-07-12 01:31:17 +0530

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import next from "next/image";
+//components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+//framer motion 
+import{motion} from 'framer-motion'
+//variants 
+import {fadeIn} from '../variants'
 const Home = () => {
   return <div>Home</div>;
 };
